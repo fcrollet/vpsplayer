d76c6df43a512914bddf45cd55cb3fd53c990239
fcrollet <fcrollet@gmail.com>
2025-12-28 11:27:02 +0100

    Update copyright

--- src/Audio_player.h
+++ src/Audio_player.h
@@ -1,4 +1,4 @@
-// Copyright 2018-2024 François CROLLET
+// Copyright 2018-2025 François CROLLET
 
 // This file is part of VPS Player.
 // VPS Player is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
